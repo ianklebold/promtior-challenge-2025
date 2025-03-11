b3803e98d09e06e1246b3b33e298842651bef73c
ianklebold <ianstgo@gmail.com>
2025-03-11 01:07:19 -0300

    argumentation step creado

---
+++ argumentation_module_service.py
@@ -0,0 +1,22 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_ollama import OllamaLLM
+
+#"llama3"
+def initialize_model(model_name):
+    return OllamaLLM(model=model_name)
+
+def get_prompt_template():
+    return ChatPromptTemplate.from_template(
+        """Please answer the question using only the information from the context below:
+        Context: {context} Question: {input}"""
+    )
+
+def chain(model, storage):
+    stuff_document_chain = create_stuff_documents_chain(model, get_prompt_template())
+    chain = create_retrieval_chain(storage.as_retriever(), stuff_document_chain)
+    return chain
+
+def execute_argumentation_step(model_name, storage):
+    return chain( initialize_model(model_name), storage )
